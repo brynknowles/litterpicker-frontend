09d165ffb361f880d91eee605ce4927c9c979e79
brynknowles <bryn.knowles@gmail.com>
2021-02-06 17:50:26 -0500

    fixed searchbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,1 +1,17 @@
-B
+
+
+function SearchBar({ searchTerm, setSearchTerm }) {
+    return (
+        <div className="searchbar">
+            <input 
+                id="search-bar"
+                type="text"
+                placeholder="Search cleanups here..."
+                value={searchTerm}
+                onChange={e => setSearchTerm(e.target.value)}
+            />
+        </div>
+    )
+}
+
+export default SearchBar
