91971af90d0d53045e2872619ec7a32c86c7b0b5
brynknowles <bryn.knowles@gmail.com>
2021-02-06 17:48:13 -0500

    searchbar changes?

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,17 +1,1 @@
-
-
-function SearchBar({ searchTerm, setSearchTerm }) {
-    return (
-        <div className="searchbar">
-            <input 
-                id="search-bar"
-                type="text"
-                placeholder="Search cleanups here..."
-                value={searchTerm}
-                onChange={e => setSearchTerm(e.target.value)}
-            />
-        </div>
-    )
-}
-
-export default SearchBar
+B
