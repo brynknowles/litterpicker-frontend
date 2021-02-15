33fffeeb4f66122f335ea3f298da3363ae50d071
brynknowles <bryn.knowles@gmail.com>
2021-02-15 14:10:10 -0500

    implemented sort by date for cleanup list

--- src/components/CleanupList.js
+++ src/components/CleanupList.js
@@ -4,7 +4,20 @@ import CleanupCard from "./CleanupCard"
 function CleanupList({ currentUser, cleanups, cleanupSearchTerm, setCleanupSearchTerm, onUpdateCheer, onUpdateCleanup, onAttendeeSignup, onDeleteCleanup }) {
     // console.log("currentUser in CleanupList: ", currentUser)
     // console.log("cleanups in CleanupList: ", cleanups)
-    const displayCleanups = cleanups.map((cleanup) => {
+
+    // movies.sort(function(a, b) {
+    //     var dateA = new Date(a.release), dateB = new Date(b.release);
+    //     return dateA - dateB;
+    // });
+
+    const sortedCleanups = cleanups.sort(function(a, b) {
+        let dateA = new Date(a.date)
+        let dateB = new Date(b.date)
+        return  dateB - dateA
+    })
+    // console.log(sortedCleanups)
+    
+    const displayCleanups = sortedCleanups.map((cleanup) => {
         return <CleanupCard 
                     key={cleanup.id}
                     currentUser={currentUser} 
@@ -15,6 +28,8 @@ function CleanupList({ currentUser, cleanups, cleanupSearchTerm, setCleanupSearc
                     onDeleteCleanup={onDeleteCleanup} 
                 />
     })
+
+    // const sortedActivities = activities.sort((a, b) => b.date - a.date)
 
     return (
         <section>
